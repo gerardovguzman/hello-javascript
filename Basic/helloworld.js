ee2b8d319842c74cc46a7a23237e4f89454daee6
gerardov <gerardovguzman1980@gmail.com>
2025-03-01 08:24:26 -0300

    Reemplazo mi archivo por el del repositorio

---
+++ Basic/helloworld.js
@@ -0,0 +1,19 @@
+console.log("¡Hola, Javascript!")
+console.log('¡Hello, Javascript!')
+//console.log('¡Hello, Javascript!')
+
+console.log(3+4)
+console.log("5")
+console.log(3*4)
+console.log(12/4)
+console.log(3-4)
+console.log(10 % 3)
+console.log(3 ** 2)
+
+// single comment
+
+/*
+many
+lines comment
+*/
+
