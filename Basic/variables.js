c38522d3dfefde8965af71ae5c2c6e88f5291ceb
gerardov <gerardovguzman1980@gmail.com>
2025-03-02 08:26:16 -0300

    Reemplazo mi archivo por el del repositorio

---
+++ Basic/variables.js
@@ -0,0 +1,21 @@
+// CREAR UNA VARIABLE 
+
+/*
+var helloWorld = "¡Hello Javascript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, Javascript!"
+console.log(helloWorld)
+*/
+
+let helloWorld2 = "¡Hello Javascript!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, Javascript!"
+console.log(helloWorld2)
+
+const helloWorld3 = "¡Hola Javascript 3!"
+console.log(helloWorld3)
+
+//helloWorld3 = "¡Esto no es permitido, porque es una constante!"
+
