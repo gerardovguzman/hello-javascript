61d5cc53b509e74d29c6f53f087fade6f5ac6c9c
gerardov <gerardovguzman1980@gmail.com>
2025-03-03 07:38:50 -0300

    File replacement

---
+++ datatypes.js
@@ -0,0 +1,45 @@
+// TIPOS DE DATOS PRIMITIVOS
+
+// String
+let myName = "Gerado Guzman"
+let alias = "gerdovguzman"
+let email = 'gerardovguzman1980@gmail.com'
+
+// Numbers
+let age = 44    // entero
+let height = 1.77    // decimal
+
+// Booleans
+let isStudent = true
+let isTeacher = false
+
+// Undefined
+let undefinedValue
+//console.log(undefinedValue)
+
+// Null
+let nullValue = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(234284728472949824247247294238498204734879797787979898789798789873)
+let myBigInt2 = 23428472847294982424724729423849820473487979778797989878979878987977979n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+console.log(typeof age)
+console.log(typeof height)
+console.log(typeof isStudent)
+console.log(typeof isTeacher)
+console.log(typeof undefinedValue)
+console.log(typeof nullValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
+
+
+
